d7d9a29838cb9386a380635b38603c7ca5e9f86a
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2023-09-01 19:41:32 +0900

    feat : 사운드 버그 수정

--- Assets/Script/Managers/Managers.cs
+++ Assets/Script/Managers/Managers.cs
@@ -14,7 +14,7 @@ public class Managers : MonoBehaviour
     ResourceManager _resource = new ResourceManager();
     SceneManagerEx _scene = new SceneManagerEx();
     //InputManager _input = new InputManager();
-    //SoundManager _sound = new SoundManager();
+    SoundManager _sound = new SoundManager();
     PoolManager _pool = new PoolManager();
     GameManagerEx _game = new GameManagerEx();
     DataManager _data = new DataManager();
@@ -24,7 +24,7 @@ public class Managers : MonoBehaviour
     public static SceneManagerEx Scene { get { return Instance._scene; } }
     public static ResourceManager Resource { get { return Instance._resource; } }
     //public static InputManager Input { get { return Instance._input; } }
-    //public static SoundManager Sound { get { return Instance._sound; } }
+    public static SoundManager Sound { get { return Instance._sound; } }
     public static PoolManager Pool { get { return Instance._pool; } }
     public static DataManager Data { get { return Instance._data; } }
 
